3c01b7dd7d33629263531c00bceab247cf99aa27
André <andre.santos01@edu.pucrs.br>
2021-09-07 17:44:17 -0300

    versao achada online ajustada para 5 filosofos

--- farm_diner.cpp
+++ farm_diner.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 std::mutex g_lockprint;
-constexpr  int no_of_philosophers = 7;
+constexpr int no_of_philosophers = 5;
 
 class sync_channel
 {
@@ -154,8 +154,8 @@ class table
          { 3, 3 },
          { 4, 4 },
          { 5, 1 },
-         { 6, 6 },
-         { 7, 1 },
+         //{ 6, 6 },
+         //{ 7, 1 },
       }
    };
 
@@ -167,8 +167,8 @@ class table
          { 3, "Descartes", setup, forks[2], forks[3] },
          { 4, "Kant",      setup, forks[3], forks[4] },
          { 5, "Nietzsche", setup, forks[4], forks[0] },
-         { 6, "Hume",      setup, forks[5], forks[6] },
-         { 7, "Russell",   setup, forks[6], forks[0] },
+         //{ 6, "Hume",      setup, forks[5], forks[6] },
+         //{ 7, "Russell",   setup, forks[6], forks[0] },
       }
    };
 
